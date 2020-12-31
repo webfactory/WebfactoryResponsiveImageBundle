dcf00625865b4c6177493333c93a5f3047c85cba
Matthias Pigulla <mp@webfactory.de>
2020-12-31 15:44:56 +0000

    Deprecate the "collection" and "instanceof" Twig tests

--- Twig/ResponsiveImageExtension.php
+++ Twig/ResponsiveImageExtension.php
@@ -22,6 +22,8 @@ final class ResponsiveImageExtension extends AbstractExtension
      */
     public function isInstanceof($object, $class)
     {
+        trigger_deprecation('webfactory/responsive-image-bundle', '1.8.2', 'The "instanceof" Twig test is deprecated and will be removed in the 2.0 version of this bundle.');
+
         return $object instanceof $class;
     }
 
@@ -31,6 +33,8 @@ final class ResponsiveImageExtension extends AbstractExtension
      */
     public function isCollection($object)
     {
+        trigger_deprecation('webfactory/responsive-image-bundle', '1.8.2', 'The "collection" Twig test is deprecated and will be removed in the 2.0 version of this bundle.');
+
         return $object instanceof \Doctrine\Common\Collections\Collection;
     }
 }
