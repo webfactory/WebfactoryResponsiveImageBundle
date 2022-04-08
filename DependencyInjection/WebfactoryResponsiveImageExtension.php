4ede7e932d551cd21b749e559797cf90e369c678
Matthias Pigulla <mp@webfactory.de>
2022-04-08 10:57:08 +0200

    Allow Symfony 5.x components to be used (#8)

--- DependencyInjection/WebfactoryResponsiveImageExtension.php
+++ DependencyInjection/WebfactoryResponsiveImageExtension.php
@@ -4,9 +4,9 @@ namespace Webfactory\ResponsiveImageBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
 class WebfactoryResponsiveImageExtension extends Extension implements PrependExtensionInterface
